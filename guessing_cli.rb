133e5434ddc32c23f53b7113fb51ceb63a96901d
Josue V. Alorro <joalorro@gmail.com>
2018-07-11 20:05:25 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
     puts "Guess a number between 1 and 6."
     input = gets.downcase.chomp
     
-    number = 1 + rand(6)
+    number = rand(1..6)
     if input.to_i == number
       puts "You guessed the correct number!"
     elsif input == "exit"
